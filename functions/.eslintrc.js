eaef5814f8f700f0af9b5061723a1a669a9ecab1
Daniel Pitt <danielpitt@Daniels-MacBook-Air.local>
2025-07-20 16:10:35 +0100

    Restore Firebase Functions with proper configuration
    - Recreated functions directory with proper package.json and TypeScript config
    - Added firebase-admin and firebase-functions dependencies
    - Fixed module resolution issues that were causing Vercel build failures
    - Functions now compile successfully and are ready for deployment

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -6,11 +6,7 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
+    "@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -18,8 +14,7 @@ module.exports = {
     sourceType: "module",
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
+    "/lib/**/*",
   ],
   plugins: [
     "@typescript-eslint",
