ac18504799eaaf9ee30a104e8db03b7ff388f1b7
Daniel Pitt <danielpitt@Daniels-MacBook-Air.local>
2025-07-20 16:20:40 +0100

    Fix Firebase Functions imports for compatibility

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,10 @@
-import {onRequest} from "firebase-functions/v2/https";
+import * as functions from "firebase-functions";
 import * as logger from "firebase-functions/logger";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((request, response) => {
   logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
